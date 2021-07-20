36d1f6eed606df4648281830794bda165d3bcd47
Steve Malers <steve.malers@openwaterfoundation.org>
2021-07-20 11:27:05 -0600

    Fix bug where FillUsingCIUFlag default was not set, issue #103

--- src/DWR/DMI/StateDMI/StateDMI.java
+++ src/DWR/DMI/StateDMI/StateDMI.java
@@ -71,7 +71,7 @@ public static final String PROGRAM_NAME = "StateDMI";
  *   As of version 5.0.8.dev, no zero-padding is used, although single zero is allowed.
  * - The version is used by build scripts to assign version to installers and documentation.
  */
-public static final String PROGRAM_VERSION = "5.1.0.dev (2021-06-22)";
+public static final String PROGRAM_VERSION = "5.1.1.dev (2021-07-20)";
 
 /**
 Interface for StateCU commands.
